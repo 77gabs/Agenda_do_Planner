0f90d5a30babcebca84110c6cfbfce600fa116d0
Letícia Vitória Vitória <115107670+letvitoria@users.noreply.github.com>
2022-11-10 21:05:58 +0000

    2

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 from tkinter import *
 import sqlite3
 from pandas import *
+
+# #Criando o Banco de Dados:
+conexao = sqlite3.connect('clientes.db')
+cor1 = '#B0C4DE'
+#arquivo = open('clientes.xlsx', '+a')
+# # Criando o cursor:
+c = conexao.cursor()
