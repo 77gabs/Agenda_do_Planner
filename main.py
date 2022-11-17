70a127c9effccf9102b5537776d2e3b4d44e51ae
VinyciusV456 <115172196+VinyciusVieira@users.noreply.github.com>
2022-11-17 14:54:31 +0000

    tela com o login e o cadastro

--- main.py
+++ main.py
@@ -7,3 +7,22 @@ from visualizar import *
 import os
 if not os.path.isfile("Bancodedados.db"):
     os.system("python banco.py")
+
+def tela_inicio():
+    janelaInicial = Tk()
+    janelaInicial.title("Planner LGYV")
+    janelaInicial.geometry("800x400")
+    janelaInicial.resizable(width=False, height=False)
+    img_telainicial = PhotoImage(file='interface/telas/telInicio.png')
+    img_botaologin = PhotoImage(file='interface/botoes/botaoLogin.png')
+    img_botaocadastro = PhotoImage(file='interface/botoes/botaoCadastro.png')
+    lab_fundo = Label(janelaInicial, image=img_telainicial)
+    lab_fundo.pack()
+    # Criação de botões
+    bt_login = Button(janelaInicial, bd=0, bg='white', image=img_botaologin,command=lambda: [janelaInicial.destroy(), tela_login()])
+    bt_login.place(width=100, height=30, x=448, y=345)
+
+    bt_cadastro = Button(janelaInicial, bd=0, bg='white', image=img_botaocadastro,command=lambda: [janelaInicial.destroy(), telCadastrar()])
+    bt_cadastro.place(width=100, height=30, x=253, y=345)
+    janelaInicial.mainloop()
+tela_inicio()
