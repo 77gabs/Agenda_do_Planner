b209052d46f8f3895dc3851c529bd20a9203771d
Yasmin Leal <115173635+yasmindopneu@users.noreply.github.com>
2022-11-17 00:24:46 +0000

    a conexão de bd, e a classe usuario.

---
+++ visualizar.py
@@ -0,0 +1,17 @@
+#SQLite
+import sqlite3 as lite
+#conexão
+conexao = lite.connect('Bancodedados.db')
+
+class usuario:
+  def __init__(self,nome,senha,telefone):
+    self.nome = nome
+    self.__senha = senha
+    self.__backup = telefone
+  def recuperar_id(self):
+    with conexao:
+      cursor = conexao.cursor()
+      query = "SELECT id FROM usuario WHERE nome == ?"
+      cursor.execute(query, [self.nome])
+      id_usuario = cursor.fetchall()
+    return id_usuario[0][0]
